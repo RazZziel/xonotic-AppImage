3df30d3955cff2bf4ea853d2f7768221e830fd4f
Brian Ting <brian@pop-os.localdomain>
2019-06-17 13:55:18 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # xonotic-AppImage
-AppImage build for Xonotic
+An AppImage build of Xonotic. Download it, make it executable, and run it.
+![Xonotic logo](img/xonotic.png)
+
+## Download
+Go to [release](https://github.com/tyu1996/opera-AppImage/releases) page to download the latest AppImage build.
+
+## How to use it
+In terminal
+
+> chmod +x ./*.AppImage
+
+In GUI
+
+![GUI](img/guiEx.png)
---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get AppImageToll
+wget -c https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage
+
+# Download Xonotic package
+wget -c https://dl.xonotic.org/xonotic-0.8.2.zip
+unzip xonotic-0.8.2.zip
+
+# Create Xonotic.AppDir and move all the content into there
+mkdir Xonotic.AppDir;mkdir Xonotic.AppDir/usr;mkdir Xonotic.AppDir/usr/bin
+mv Xonotic/* Xonotic.AppDir/usr/bin
+rmdir Xonotic
+
+# Get more contents for AppDir
+unzip AppDirContents.zip -d Xonotic.AppDir
+
+# Start building
+ARCH=x86_64 ./appimagetool-x86_64.AppImage Xonotic.AppDir/
